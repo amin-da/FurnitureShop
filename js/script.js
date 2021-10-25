1c5c7a4c083807c1dead7197e2a674c769e123bb
amin-da <darabniya@yahoo.com>
2021-10-25 11:30:47 +0330

    add home section in project

--- js/script.js
+++ js/script.js
@@ -50,3 +50,19 @@ searchBtn.onclick = () => {
 window.onscroll = () => {
   searchForm.classList.remove("active");
 };
+
+//home section slider
+let slides = document.querySelectorAll(".home .slides-container .slide");
+let index = 0;
+
+function nextSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+function prevSlide() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
